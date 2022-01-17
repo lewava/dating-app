87460d4881c930ca8a93885427a85d2ac1f897b2
Harshada-Mulye <mulyeharshada@gmail.com>
2022-01-17 15:05:14 +0100

    startpage added

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,8 +1,28 @@
-
+import { Link } from 'react-router-dom';
+import styles from "../styles/startpage.scss";
+import pic from "../assets/image/dating.jpg";
 const StartPage = () => {
   return (
     <div>
-      StartPage
+		<div>
+		<nav className='startpageNav'>
+			<Link to='/' className="NavTitle">IDate</Link>
+			<Link to='Login' className="NavItem">Login</Link>
+			<Link to='user'  className="NavItem">SignUp</Link>
+		</nav>
+		</div>
+		<div className='container'>
+		
+				<h1 className="datingheading">Its never late to find love...</h1>
+      
+			
+			
+             <img src={pic} className="datingImage"></img>
+			
+			
+
+		</div>
+   
     </div>
   )
 }
