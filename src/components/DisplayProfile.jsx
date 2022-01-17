09ebec0e6f4854eb6247ace7edcbb55e8405b51e
email-90 <70515786+email-90@users.noreply.github.com>
2022-01-17 15:03:11 +0100

    form fix

--- src/components/DisplayProfile.jsx
+++ src/components/DisplayProfile.jsx
@@ -2,11 +2,12 @@ import './../components/profile.css';
 import ProfileField from './../components/ProfileField';
 
 const DisplayProfile= ({editProfile, switchMode}) => {
+
     return (
       <div>
 
         <section>
-            <article><img src="https://media.istockphoto.com/photos/businessman-silhouette-as-avatar-or-default-profile-picture-picture-id476085198?k=20&m=476085198&s=170667a&w=0&h=FXkT-N6vISLOCUefa9MyQg0pH-6loMX9zBZjgLK458c="/></article>
+            <article><img src="https://media.istockphoto.com/photos/businessman-silhouette-as-avatar-or-default-profile-picture-picture-id476085198?k=20&m=476085198&s=170667a&w=0&h=FXkT-N6vISLOCUefa9MyQg0pH-6loMX9zBZjgLK458c=" alt='#'/></article>
             <article>
                 <span>Name</span><span><ProfileField content="John Doe" editProfile={editProfile} id="name" type="text"/></span>
                 <span>Age</span><span><ProfileField content="33" editProfile={editProfile} id="age" type="number"/></span>
--- src/components/ProfileField.jsx
+++ src/components/ProfileField.jsx
@@ -1,15 +1,24 @@
 import { useState } from 'react'
-import { useEffect } from "react";
 
 
 const ProfileField= ({content, editProfile, id, type}) => {
 
   const [name, setName] = useState(content);
 
+  // function saveToLocaleStorage() {
+  //   const profile = {
+  //     name: name,
+  //     age: 'test',
+  //     location: 'test',
+  //     interest: 'test'
+  //   }
+  // }
 
     return (
       <>
-        {editProfile ? <input type={type} id={id} name={id} value={name} onChange={e => e.target.value}/> : content}
+        {editProfile ? <input type={type} id={id} name={id} value={name} onChange={e => setName(e.target.value)} 
+        // onBlur={changeName}
+        /> : content}
       </>
     );
   };
