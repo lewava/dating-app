b092e5ad8807e335dcc3bf46f6bcb1c2fe73f26d
email-90 <70515786+email-90@users.noreply.github.com>
2022-01-18 09:38:12 +0100

    small fix in scss

--- src/components/LoginUser.jsx
+++ src/components/LoginUser.jsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './login-user.scss'
 
+//Logout function
 export function logOut() {localStorage.removeItem("activeUser");}
 
 const LoginUser = () => {
