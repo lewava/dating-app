cf9eb363856667f9b079fe0d7e30c2efcea24fa2
lewava <leon.wass.vallin@gmail.com>
2022-01-18 11:59:26 +0100

    dashboard done

--- src/components/dashboard/DashboardCard.jsx
+++ src/components/dashboard/DashboardCard.jsx
@@ -1,15 +1,44 @@
-import userImage from "../../assets/image/user.svg";
+import { useState } from "react";
 import styles from "./DashboardCard.module.scss";
+import userImage from "../../assets/image/user.svg";
+import heart from "../../assets/image/heart.png";
 
-const DashboardCard = () => {
+const DashboardCard = ({ users }) => {
+  const [currentUser, setCurrentUser] = useState(0);
+
+  const currentUserHandler = () => {
+    setCurrentUser((prevState) => {
+      if (prevState === users.length - 1) return 0;
+      return prevState + 1;
+    });
+  };
+
   return (
-    <div className={styles.container}>
-      <h3>Namn</h3>
-      <div className={styles.imgContainer}>
-        <img src={userImage} alt="user" className={styles.img} />
-        <span>40år from stockholm</span>
+    <>
+      <div className={styles.container}>
+        <h2 className={styles.name}>{users[currentUser].profile.name}</h2>
+        <div className={styles.imgContainer}>
+          <img src={userImage} alt="user" className={styles.img} />
+        </div>
+        <span className={styles.info}>
+          {users[currentUser].profile.age}år från{" "}
+          {users[currentUser].profile.location}
+        </span>
       </div>
-    </div>
+      <div className={styles.btnContainer}>
+        <button className={styles.btn} onClick={currentUserHandler}>
+          No
+        </button>
+        <button className={styles.btn}>
+          <img
+            src={heart}
+            alt="yes"
+            className={styles.btnImg}
+            onClick={currentUserHandler}
+          />
+        </button>
+      </div>
+    </>
   );
 };
 
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,9 +3,14 @@ import DashboardCard from "../components/dashboard/DashboardCard";
 import styles from "../styles/Dashboard.module.scss";
 
 const Dashboard = () => {
+  const activeUser = JSON.parse(localStorage.getItem("activeUser"));
+  const users = JSON.parse(localStorage.getItem("users"));
+
+  const filteredUsers = users.filter((user) => user.email !== activeUser.email);
+
   return (
     <div className={styles.container}>
-      <DashboardCard />
+      <DashboardCard users={filteredUsers} />
     </div>
   );
 };
